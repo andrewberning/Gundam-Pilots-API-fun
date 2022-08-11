8fb6d1d5caa91a2fd24385ec3decf138504f7909
berningan <102753394+berningan@users.noreply.github.com>
2022-08-11 14:34:08 -0700

    fixed errors

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 document.querySelector('button').addEventListener('click', apiRequest)
 
-async function apiRequest() {
-  const suitsName = document.querySelector('input').value;
-  try {
-    const response = await fetch();
-    const data = await response.json();
+async function apiRequest(){
+    const suitName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://gundam-pilots-api-fun.herokuapp.com/api/${suitName}`)
+        const data = await response.json()
 
-    console.log(data);
-    document.querySelector('h2').textContent = data.pilotName;
-  }
-  catch(error) {
-    console.log(error);
-  }
+        console.log(data)
+        document.querySelector('h2').innerText = data.pilotName
+        document.querySelector('h3').innerText = data.gundamSuit
+    }catch(error){
+        console.log(error)
+    }
 }
